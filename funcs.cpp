416f6804e547789abaa381c6663d5e36e6bb251a
Xinyi Lin <xlin6798@gmail.com>
2019-10-29 14:33:36 -0400

    updates funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
 #include <cctype>
 #include "funcs.h"
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+
+std::string fixFormat(std::string str)
+{
+  std::ifstream fin(str);
+  if (fin.fail()){
+    std::cerr << "File cannot be opened for reading." << '\n';  //output error if file not found or cannot be opened
+    exit(1);
+  }
+  std::string name = "";
+  int indent = 0;
+  int temp = 0;
+  std::string ret;
+  std::string retLine = "";
+  while (std::getline(fin, name))
+  {
+    if (name != "")
+    {
+      name = removeLeadingSpaces(name);
+      ret = "";
+      if (countChar (name, '}') > countChar (name, '{'))
+        indent -= countChar (name, '}') + countChar (name, '{');
+      else if (countChar (name, '}') == countChar (name, '{') && name.find('{') > name.find('}'))
+          indent -= countChar (name, '}');
+      temp = indent;
+      while (temp > 0)
+      {
+        ret += '\t';
+        temp --;
+      }
+      if (countChar (name, '}') < countChar (name, '{') || (name.find('{') > name.find('}') && countChar (name, '}') == countChar (name, '{')))
+      {
+          indent = indent + countChar (name, '{');
+          if (countChar (name, '}') < countChar (name, '{'))
+            indent -= countChar (name, '}');
+      }
+      retLine += ret + name + '\n';
+    }
+    else
+      retLine += '\n';
+  }
+  return retLine;
+}
 
 std::string removeLeadingSpaces(std::string line) 
 {
   int i = 0;
-  while (isspace(line.at(i)))
+  while (isspace(line.at(i)) && i < line.size() - 1)
   {
     i++;
   }
@@ -22,3 +67,4 @@ int countChar(std::string line, char c)
   }
   return count;
 }
+
